d621331d661cdd62940504e4f1bec9edeb2c7603
carlosyasu91 <carlosyasu91@gmail.com>
2016-02-08 15:17:38 -0800

    Force redirects to login page when user not logged in

--- shortly.js
+++ shortly.js
@@ -2,7 +2,8 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
+var cookieParser = require( 'cookie-parser' );
+var session = require( 'express-session' );
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -20,20 +21,32 @@ app.use(partials());
 app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use( cookieParser( 'short.ly' ) );
+app.use( session( { secret: 'short.ly' } ) );
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+var restrict = function( request, response, next ) {
+  if( request.session.user ) {
+    next();
+  } else {
+    request.session.error = 'Please log in.';
+    response.redirect( '/login' );
+  }
+};
+
+
+app.get('/', restrict,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create', restrict,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links', restrict,
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
@@ -75,8 +88,9 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
-
-
+app.get( '/login', function( request, response ) {
+  response.render( 'login' );
+} );
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
